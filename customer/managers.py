9d0815dcc3caa6ed1d16623e1b59095ad4c44a07
mahdizo <mahdizolfaghari3181@gmail.com>
2022-02-26 21:04:55 +0330

    refactor : customer manager

--- customer/managers.py
+++ customer/managers.py
@@ -2,21 +2,23 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password, phone, fullname, ):
+    def create_user(self, email, password, phone, first_name, last_name):
         if not email:
             raise ValueError("users must have email")
-        if not fullname:
-            raise ValueError("users must have full name")
+        if not first_name:
+            raise ValueError("users must have first name")
+        if not last_name:
+            raise ValueError("users must have last name")
         if not phone:
             raise ValueError("users must have phone")
 
-        user = self.model(email=self.normalize_email(email), fullname=fullname, phone=phone)
+        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name, phone=phone)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, phone, fullname):
-        user = self.create_user(email, password, phone, fullname)
+    def create_superuser(self, email, password, phone, first_name, last_name):
+        user = self.create_user(email, password, phone, first_name, last_name)
         user.is_admin = True
         user.save()
         return user
