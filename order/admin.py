02d9cd87b4f090dc596960d812714b3594ef1261
mahdizo <mahdizolfaghari3181@gmail.com>
2022-04-17 12:08:22 +0430

    refactor : order admin

--- order/admin.py
+++ order/admin.py
@@ -7,13 +7,19 @@ from .models import OffCode, Order, OrderItem
 @admin.register(OffCode)
 class OffCodeAdmin(admin.ModelAdmin):
     fields = ['code', 'value', 'type', 'max_price']
+    list_display = ['value', 'type', 'max_price']
+    search_fields = ['type']
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    fields = ['customer_id', 'amount', ]
+    fields = ['customer_id', 'address', 'off_code', ]
+    list_display = ['customer_id', 'address', 'off_code', ]
+    search_fields = ['customer_id']
 
 
 @admin.register(OrderItem)
 class OrderAdmin(admin.ModelAdmin):
     fields = ['order_id', 'product', 'quantity']
+    list_display = ['quantity', "product", 'order_id']
+    search_fields = ['product']
