c70d7b697e0c2a3117f52d616a754cf5651b0c6f
mahdizo <mahdizolfaghari3181@gmail.com>
2022-04-17 12:12:00 +0430

    feat : product view update item

--- product/views.py
+++ product/views.py
@@ -1,5 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from order.models import Order, OrderItem
 from .models import Product, Category
+import json
 
 
 def home(request, slug=None):
@@ -14,3 +18,22 @@ def home(request, slug=None):
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'product/product_detail.html', {'product': product})
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    user = data['user']
+    if user == 'AnonymousUser':
+        messages.success(request, '.برای ثبت سفارش ابتدا باید وارد اکانتت خود شوید', 'warning')
+    else:
+        customer = request.user
+        print(customer)
+        productID = data['productID']
+        # action = data['action']
+        product = Product.objects.get(id=productID)
+
+        # order, created = Order.objects.get_or_create(customer_id=customer)
+        order = Order.objects.create(customer_id=customer)
+
+        OrderItem.objects.get_or_create(order_id=order, product=product, customer_id=customer)
+    return JsonResponse('Item was added', safe=False)
