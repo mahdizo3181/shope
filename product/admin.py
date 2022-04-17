6bed2f309c31a7ea05b34ceceb0352e27e05c8cf
mahdizo <mahdizolfaghari3181@gmail.com>
2022-04-17 12:11:19 +0430

    refactor : admin product

--- product/admin.py
+++ product/admin.py
@@ -7,6 +7,7 @@ class CategoryAdmin(admin.ModelAdmin):
     fields = ['name', 'category_id', 'is_sub', 'slug']
     list_display = ['name', 'category_id', 'is_sub', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
 
 
 @admin.register(Product)
@@ -17,8 +18,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'discount_id', 'brand', 'inventory', 'image',
                     'available', ]
     prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
     fields = ['value', 'type', 'max_price']
+    list_display = ['value', 'type', 'max_price']
+    search_fields = ['type']
